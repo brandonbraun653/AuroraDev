2da5b911fb413d964a14fce10d53fec57f0a5c28
Brandon Braun <brandonbraun653@gmail.com>
2021-11-21 13:46:53 -0700

    Fully test MPMCQueue

--- tests/container/fixture_mpmc_queue.hpp
+++ tests/container/fixture_mpmc_queue.hpp
@@ -15,12 +15,18 @@
 /*-----------------------------------------------------------------------------
 Includes
 -----------------------------------------------------------------------------*/
+#include <cstdlib>
 #include <Aurora/container>
 #include "gtest/gtest.h"
 
 
 namespace Aurora::Test::MPMCQueue
 {
+  /*---------------------------------------------------------------------------
+  Constants
+  ---------------------------------------------------------------------------*/
+  static constexpr size_t MIN_TEST_ELEMENTS = 10;
+
   /*---------------------------------------------------------------------------
   Structures
   ---------------------------------------------------------------------------*/
@@ -39,11 +45,39 @@ namespace Aurora::Test::MPMCQueue
   protected:
     virtual void SetUp()
     {
+      test_sig = Chimera::Interrupt::SIGNAL_INVALID;
+      static_init = rand() % 2;
+      if( static_init )
+      {
+        static_init = false;
+
+        GTEST_ASSERT_EQ( test_circ_buf.init( test_array, ARRAY_COUNT( test_array ) ), true );
+        GTEST_ASSERT_EQ( test_attr.init( &test_circ_buf, &test_mtx, test_sig ), true );
+      }
+      else
+      {
+        static_init = true;
+        GTEST_ASSERT_EQ( test_attr.init( ( rand() % 100 ) + MIN_TEST_ELEMENTS ), true );
+      }
+
+
+      GTEST_ASSERT_EQ( true, test_queue.init( test_attr ) );
     }
 
     virtual void TearDown()
     {
+      test_attr.destroy();
     }
+
+    Aurora::Container::MPMCAttr<TestObj> test_attr;
+    Aurora::Container::MPMCQueue<TestObj> test_queue;
+    Chimera::Interrupt::Signal_t test_sig;
+
+  private:
+    bool static_init;
+    TestObj test_array[ MIN_TEST_ELEMENTS + 50 ];
+    Aurora::Container::CircularBuffer<TestObj> test_circ_buf;
+    Chimera::Thread::RecursiveMutex test_mtx;
   };
 }  // namespace Aurora::Test::Container
 
--- tests/container/test_mpmc_queue.cpp
+++ tests/container/test_mpmc_queue.cpp
@@ -24,4 +24,94 @@ namespace Aurora::Test::MPMCQueue
 
     GTEST_ASSERT_EQ( false, queue.init( attr ) );
   }
+
+  TEST( MPMC_RobustTests, AttrNullElements )
+  {
+    auto attr = Aurora::Container::MPMCAttr<TestObj>();
+    GTEST_ASSERT_EQ( false, attr.init( 0 ) );
+  }
+
+  TEST( MPMC_RobustTests, AttrMissingMem )
+  {
+    auto cb   = Aurora::Container::CircularBuffer<TestObj>();
+    auto mtx  = Chimera::Thread::RecursiveMutex();
+    auto sig  = Chimera::Interrupt::SIGNAL_INVALID;
+    auto attr = Aurora::Container::MPMCAttr<TestObj>();
+
+    GTEST_ASSERT_EQ( false, attr.init( nullptr, nullptr, sig ) );
+    GTEST_ASSERT_EQ( false, attr.init( nullptr, &mtx, sig ) );
+    GTEST_ASSERT_EQ( false, attr.init( &cb, nullptr, sig ) );
+  }
+
+  TEST_F( MPMCFixture, GeneralSizeAttributes )
+  {
+    GTEST_ASSERT_EQ( true, test_attr.valid() );
+    GTEST_ASSERT_EQ( true, test_queue.empty() );
+    GTEST_ASSERT_EQ( true, test_queue.empty( false ) );
+    GTEST_ASSERT_GE( test_queue.capacity(), MIN_TEST_ELEMENTS );
+    GTEST_ASSERT_GE( test_queue.capacity( false ), MIN_TEST_ELEMENTS );
+    GTEST_ASSERT_EQ( test_queue.available(), test_queue.capacity() );
+    GTEST_ASSERT_EQ( test_queue.available(), test_queue.capacity( false ) );
+    GTEST_ASSERT_EQ( 0, test_queue.size() );
+    GTEST_ASSERT_EQ( 0, test_queue.size( false ) );
+  }
+
+  TEST_F( MPMCFixture, PushBadInputs )
+  {
+    TestObj dummy;
+    GTEST_ASSERT_EQ( test_queue.push( nullptr, 10 ), 0 );
+    GTEST_ASSERT_EQ( test_queue.push( &dummy, 0 ), 0 );
+  }
+
+  TEST_F( MPMCFixture, PopBadInputs )
+  {
+    TestObj dummy;
+    GTEST_ASSERT_EQ( test_queue.pop( nullptr, 10 ), 0 );
+    GTEST_ASSERT_EQ( test_queue.pop( &dummy, 0 ), 0 );
+  }
+
+  TEST_F( MPMCFixture, PushPopConditions )
+  {
+    TestObj dummy;
+    dummy.item0 = 1.0f;
+
+    /*-------------------------------------------------------------------------
+    Validate pushing until full is ok
+    -------------------------------------------------------------------------*/
+    while( !test_queue.full() )
+    {
+      GTEST_ASSERT_EQ( test_queue.push( &dummy, 1 ), 1 );
+    }
+
+    /*-------------------------------------------------------------------------
+    Once full, validate reported sizing
+    -------------------------------------------------------------------------*/
+    GTEST_ASSERT_EQ( test_queue.full(), true );
+    GTEST_ASSERT_EQ( test_queue.size(), test_queue.capacity() );
+
+    /*-------------------------------------------------------------------------
+    Try to push one more time and validate it fails
+    -------------------------------------------------------------------------*/
+    GTEST_ASSERT_EQ( test_queue.push( &dummy, 1 ), 0 );
+
+    /*-------------------------------------------------------------------------
+    Pop all the data off
+    -------------------------------------------------------------------------*/
+    while( !test_queue.empty() )
+    {
+      GTEST_ASSERT_EQ( test_queue.pop( &dummy, 1 ), 1 );
+    }
+
+    /*-------------------------------------------------------------------------
+    Once empty, validate sizing
+    -------------------------------------------------------------------------*/
+    GTEST_ASSERT_EQ( test_queue.full(), false );
+    GTEST_ASSERT_EQ( test_queue.empty(), true );
+    GTEST_ASSERT_EQ( test_queue.size(), 0 );
+
+    /*-------------------------------------------------------------------------
+    Pop one more time to validate empty behavior
+    -------------------------------------------------------------------------*/
+    GTEST_ASSERT_EQ( test_queue.pop( &dummy, 1 ), 0  );
+  }
 }
